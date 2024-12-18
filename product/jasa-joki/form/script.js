d066c2d316f8196c95a608f209d218c5d8801e6e
kabureneo <kaburaneo@gmail.com>
2024-12-18 15:17:36 +0700

    sedikit uji coba pada bagian product

--- product/jasa-joki/form/script.js
+++ product/jasa-joki/form/script.js
@@ -1,17 +1,17 @@
 //meambil data dari html
 const container = document.querySelector('.container');
 
-        // Delegasi event pada container
-        container.addEventListener('click', (event) => {
-            // Periksa apakah elemen yang diklik adalah div dengan class clickable-div
-            if (event.target.classList.contains('wrapper')) {
-                // Ambil id checkbox dari atribut data-target
-                const targetCheckboxId = event.target.getAttribute('data-target');
-                const checkbox = document.getElementById(targetCheckboxId);
+// Delegasi event pada container
+container.addEventListener('click', (event) => {
+    // Periksa apakah elemen yang diklik adalah div dengan class clickable-div
+    if (event.target.classList.contains('wrapper')) {
+        // Ambil id checkbox dari atribut data-target
+        const targetCheckboxId = event.target.getAttribute('data-target');
+        const checkbox = document.getElementById(targetCheckboxId);
 
-                // Toggle checkbox jika ditemukan
-                if (checkbox) {
-                    checkbox.checked = !checkbox.checked;
-                }
-            }
-        });
+        // Toggle checkbox jika ditemukan
+        if (checkbox) {
+            checkbox.checked = !checkbox.checked;
+        }
+    }
+});
